188b66bf3d9cf735aa8706b8da3518dafea83dee
satwika16 <satwikakarra@gmail.com>
2023-08-23 11:13:23 +0530

    migrated professor menu to professor REST Controller

--- CRS-GROUPD-WIBMO-REST/src/main/java/com/wibmo/application/CrsGroupdWibmoRestApplication.java
+++ CRS-GROUPD-WIBMO-REST/src/main/java/com/wibmo/application/CrsGroupdWibmoRestApplication.java
@@ -1,9 +1,17 @@
 package com.wibmo.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@EnableWebMvc
+@Configuration
 public class CrsGroupdWibmoRestApplication {
 
 	public static void main(String[] args) {
