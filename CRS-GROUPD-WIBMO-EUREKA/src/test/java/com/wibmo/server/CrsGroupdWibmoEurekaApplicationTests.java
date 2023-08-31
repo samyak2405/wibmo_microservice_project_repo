0b8b7b1e01a6103d2d04e4747d320f3bd6c7002f
samyak2405 <moonsamyak54@gmail.com>
2023-08-31 16:45:18 +0530

    Initialized Microservices project

---
+++ CRS-GROUPD-WIBMO-EUREKA/src/test/java/com/wibmo/server/CrsGroupdWibmoEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wibmo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrsGroupdWibmoEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ CRS-GROUPD-WIBMO-STUDENT-MICROSERVICE-1/src/test/java/com/wibmo/application/CrsGroupdWibmoStudentMicroservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wibmo.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrsGroupdWibmoStudentMicroservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
