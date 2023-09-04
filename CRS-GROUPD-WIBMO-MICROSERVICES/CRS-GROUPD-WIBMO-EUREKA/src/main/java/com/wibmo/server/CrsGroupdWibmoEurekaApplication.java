0674fa2392f5024019a309044b01cdbff2fdbe22
samyak2405 <moonsamyak54@gmail.com>
2023-09-04 11:03:40 +0530

    Added Required annotations on main class of various services

--- CRS-GROUPD-WIBMO-MICROSERVICES/CRS-GROUPD-WIBMO-EUREKA/src/main/java/com/wibmo/server/CrsGroupdWibmoEurekaApplication.java
+++ CRS-GROUPD-WIBMO-MICROSERVICES/CRS-GROUPD-WIBMO-EUREKA/src/main/java/com/wibmo/server/CrsGroupdWibmoEurekaApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 @EnableEurekaServer
 public class CrsGroupdWibmoEurekaApplication {
 
