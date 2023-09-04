ab93c9d9d59151ffde646a032c3e802ca7733534
samyak2405 <moonsamyak54@gmail.com>
2023-09-04 17:29:17 +0530

    Added Swagger

--- CRS-GROUPD-WIBMO-MICROSERVICES/CRS-GROUPD-WIBMO-AUTHENTICATION/src/main/java/com/wibmo/application/CrsGroupdWibmoAuthenticationApplication.java
+++ CRS-GROUPD-WIBMO-MICROSERVICES/CRS-GROUPD-WIBMO-AUTHENTICATION/src/main/java/com/wibmo/application/CrsGroupdWibmoAuthenticationApplication.java
@@ -5,11 +5,17 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableJpaRepositories("com.wibmo.repository")
@@ -25,6 +31,23 @@ public class CrsGroupdWibmoAuthenticationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrsGroupdWibmoAuthenticationApplication.class, args);
+	}
+	@Bean
+
+	public Docket apiDocket() {
+
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+
+				.paths(PathSelectors.any()).build();
+
+	}
+
+	@Bean
+
+	public InternalResourceViewResolver defaultViewResolver() {
+
+		return new InternalResourceViewResolver();
+
 	}
 
 }
--- CRS-GROUPD-WIBMO-MICROSERVICES/CRS-GROUPD-WIBMO-STUDENT-MICROSERVICE/src/main/java/com/wibmo/application/CrsGroupdWibmoStudentMicroserviceApplication.java
+++ CRS-GROUPD-WIBMO-MICROSERVICES/CRS-GROUPD-WIBMO-STUDENT-MICROSERVICE/src/main/java/com/wibmo/application/CrsGroupdWibmoStudentMicroserviceApplication.java
@@ -5,11 +5,17 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -28,6 +34,23 @@ public class CrsGroupdWibmoStudentMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrsGroupdWibmoStudentMicroserviceApplication.class, args);
+	}
+	@Bean
+
+	public Docket apiDocket() {
+
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+
+				.paths(PathSelectors.any()).build();
+
+	}
+
+	@Bean
+
+	public InternalResourceViewResolver defaultViewResolver() {
+
+		return new InternalResourceViewResolver();
+
 	}
 
 }
