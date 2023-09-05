610a077877c7a53002adcdac1d5f73a371ed8538
satwika16 <satwikakarra@gmail.com>
2023-09-05 10:05:20 +0530

    Resolved conflicts

--- CRS-GROUPD-WIBMO-MICROSERVICES/CRS-GROUPD-WIBMO-STUDENT-MICROSERVICE/src/main/java/com/wibmo/application/CrsGroupdWibmoStudentMicroserviceApplication.java
+++ CRS-GROUPD-WIBMO-MICROSERVICES/CRS-GROUPD-WIBMO-STUDENT-MICROSERVICE/src/main/java/com/wibmo/application/CrsGroupdWibmoStudentMicroserviceApplication.java
@@ -5,11 +5,17 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -29,5 +35,5 @@ public class CrsGroupdWibmoStudentMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrsGroupdWibmoStudentMicroserviceApplication.class, args);
 	}
-
+	
 }
